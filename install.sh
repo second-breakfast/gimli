e532d72f7235ae27aff7bb2fab8cb5635094c9dc
Peregrin Took <second-breakfast@users.noreply.github.com>
2019-07-04 03:12:39 +0000

    Closes #10
    Have install.sh copy the gimli binary to /usr/local/bin.

--- install.sh
+++ install.sh
@@ -1,4 +1,43 @@
 #!/bin/sh
 
-pip3 install .
-cp bin/gimli /usr/local/bin
+#==================
+#    FUNCTIONS    #
+#==================
+
+# Ask if user wants to continue, exit if not.
+confirm() {
+    read -p "Continue? (y/N): " c
+    if [ "$c" = "" -o "$c" = "n" -o "$c" = "N" ]; then
+        exit 1
+    elif [ "$c" = "y" -o "$c" = "Y" ]; then
+        continue
+    else
+        printf "Invalid response, exiting...\n"
+        exit 1
+    fi
+}
+
+#==================
+#  MAIN PROGRAM   #
+#==================
+
+# Install gimli via the local PyPi package in this directory.
+pip3 install --upgrade .
+
+# Copy gimli binary to $HOME/bin (ask to create $HOME/bin if it doesn't exist).
+if [ -d $HOME/bin ]; then
+    cp bin/gimli $HOME/bin/
+else
+    printf "\nNo bin folder at $HOME/bin. Would you like to create it?\n"
+    confirm
+    mkdir $HOME/bin > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        printf "error: Unable to create $HOME/bin\n"
+        exit 1
+    fi
+    cp bin/gimli $HOME/bin/
+fi
+
+printf "\nThe gimli binary has been installed to $HOME/bin/gimli.\n"
+printf "\nTo run gimli, do:\n\n"
+printf "  export PATH=\$PATH:\$HOME/bin\n  gimli -h\n\n"
