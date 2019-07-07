85cea6c4b3ff26701913da1d99046fb544dcf6cd
Peregrin Took <second-breakfast@users.noreply.github.com>
2019-07-07 04:07:01 +0000

    Update tests for new cli

--- test/test.py
+++ test/test.py
@@ -4,8 +4,12 @@ from gimli import Gimli
 g = Gimli()
 
 start_time = time.time()
-g.cpu_util()
-print("cpu_util took {} seconds".format(time.time() - start_time))
+g.cpustat()
+print("cpustat took {} seconds".format(time.time() - start_time))
+
+start_time = time.time()
+g.cputot()
+print("cputot took {} seconds".format(time.time() - start_time))
 
 start_time = time.time()
 g.meminfo()
@@ -14,11 +18,3 @@ print("meminfo took {} seconds".format(time.time() - start_time))
 start_time = time.time()
 g.memusage()
 print("memusage took {} seconds".format(time.time() - start_time))
-
-start_time = time.time()
-g.cpu_util_str()
-print("cpu_util_str took {} seconds".format(time.time() - start_time))
-
-start_time = time.time()
-g.memusage_str()
-print("memusage_str took {} seconds".format(time.time() - start_time))
