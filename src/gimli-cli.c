c81e48cab5cf586916a645106517e4e840f204f9
Peregrin Took <second-breakfast@users.noreply.github.com>
2020-03-25 23:52:10 -0600

    Add interactive input to gimli-cli

--- src/gimli-cli.c
+++ src/gimli-cli.c
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include <string.h>
 
 int
 gimli_connect(char *host, int port)
@@ -29,26 +30,51 @@ main(int argc, char **argv)
     int fd;
     int len;
     char buf[1024];
+    char *input = NULL;
+    char *host, *port;
 
-    if (argc != 3) {
+    if (!(argc == 3 || argc == 1)) {
         printf("gimli-cli <host> <port>\n");
         return (1);
     }
 
-    fd = gimli_connect(argv[1], strtol(argv[2], &(char *){ "gimli" }, 10));
+    if (argc == 3) {
+        host = strdup(argv[1]);
+        port = strdup(argv[2]);
+        fd = gimli_connect(argv[1], strtol(argv[2], &(char *){ "" }, 10));
+    } else {
+        host = strdup("127.0.0.1");
+        port = strdup("8001");
+        fd = gimli_connect("127.0.0.1", 8001);
+    }
 
-    // while (1) {
-        if (send(fd, "gimli", sizeof ("gimli"), 0) < 0) {
+    while (1) {
+        printf("%s:%s> ", host, port);
+        if (getline(&input, &(int) { 0 }, stdin) == -1) {
             return (2);
         }
-        if ((len = recv(fd, buf, 256, 0)) < 1) {
+
+        if (strncmp(input, "quit", strlen("quit")) == 0 ||
+            strncmp(input, "exit", strlen("exit")) == 0) {
+            return (0);
+        }
+
+        if (send(fd, input, strlen(input), 0) < 0) {
+            free(input);
             return (3);
+        }
+        if ((len = recv(fd, buf, 256, 0)) < 1) {
+            free(input);
+            return (4);
         }
         buf[len - 1] = '\0';
         printf("%s\n", buf);
+        free(input);
         // printf("%s\r", buf);
         // fflush(stdout);
-    // }
+    }
 
+    free(host);
+    free(port);
     return (0);
 }
